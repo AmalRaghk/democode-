de59f674fd6971eb213b3a98563518ba14e2f691
anjush <you@example.com>
2021-05-18 01:52:25 +0530

    c fundamentals

---
+++ bus.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{printf("hello welcome to the world of codding\n" "start your coding\n ");
+ int i=1,j=1;
+ for(i=1,j=1;i<=10,j<=9;i++,j++)
+  {printf("\n%i=%2i \n",i,j);
+    
+   }
+ printf("\nthe end\n");
+
+}
